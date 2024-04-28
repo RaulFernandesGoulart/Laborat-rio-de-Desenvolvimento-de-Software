deb0af19da4927c29ff24b0cf1737bd838c0e860
Raul Fernandes <103540662+RaulFernandesGoulart@users.noreply.github.com>
2024-04-28 20:26:18 -0300

    Update ErroMessage.java

---
+++ roteiro01/src/main/java/com/model/ErroMessage.java
@@ -0,0 +1,9 @@
+package com.labdessoft.roteiro01.model;
+
+public class ErrorMessage {
+    public final String message;
+
+    public ErrorMessage(String message) {
+        this.message = message;
+    }
+}
