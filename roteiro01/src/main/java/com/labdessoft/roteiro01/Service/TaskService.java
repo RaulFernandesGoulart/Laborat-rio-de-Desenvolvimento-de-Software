861ca4eca1317d894f81704003e4187384725d9c
Raul Fernandes <103540662+RaulFernandesGoulart@users.noreply.github.com>
2024-04-28 20:18:29 -0300

    Update TaskService.java

--- roteiro01/src/main/java/com/labdessoft/roteiro01/Service/TaskService.java
+++ roteiro01/src/main/java/com/labdessoft/roteiro01/Service/TaskService.java
@@ -1,17 +1,16 @@
 package com.labdessoft.roteiro01.service;
 
 import com.labdessoft.roteiro01.entity.Task;
+import com.labdessoft.roteiro01.entity.TaskStatus;
 import com.labdessoft.roteiro01.entity.TaskType;
 import com.labdessoft.roteiro01.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class TaskService {
@@ -20,40 +19,23 @@ public class TaskService {
     private TaskRepository taskRepository;
 
     public Task obterTarefaPorId(Long id) {
-        Optional<Task> taskOptional = taskRepository.findById(id);
-        if (taskOptional.isPresent()) {
-            Task task = taskOptional.get();
-            setTaskStatus(task);
-            return task;
-        }
-        return null;
+        return taskRepository.findById(id)
+                .map(this::setTaskStatus)
+                .orElse(null);
     }
 
     public Task criarTarefa(Task tarefa) {
-        if (tarefa.getType() == TaskType.DATA && tarefa.getDueDate().isBefore(LocalDate.now())) {
-            throw new IllegalArgumentException("A data de vencimento para tarefas do tipo 'Data' deve ser igual ou após a data atual.");
-        }
-        setTaskStatus(tarefa);
-        return taskRepository.save(tarefa);
+        validarDataDeVencimento(tarefa);
+        return taskRepository.save(setTaskStatus(tarefa));
     }
 
     public Task atualizarTarefa(Long id, Task tarefaAtualizada) {
-        Optional<Task> tarefaOptional = taskRepository.findById(id);
-        if (tarefaOptional.isPresent()) {
-            Task tarefaExistente = tarefaOptional.get();
-            // Atualizar propriedades comuns
-            tarefaExistente.setDescription(tarefaAtualizada.getDescription());
-            tarefaExistente.setCompleted(tarefaAtualizada.getCompleted());
-            // Atualizar propriedades específicas do tipo
-            tarefaExistente.setType(tarefaAtualizada.getType());
-            tarefaExistente.setDeadlineInDays(tarefaAtualizada.getDeadlineInDays());
-            tarefaExistente.setDueDate(tarefaAtualizada.getDueDate());
-            tarefaExistente.setPriority(tarefaAtualizada.getPriority());
-            setTaskStatus(tarefaExistente); // Atualizar status
-            return taskRepository.save(tarefaExistente);
-        } else {
-            return null;
-        }
+        return taskRepository.findById(id)
+                .map(tarefaExistente -> {
+                    tarefaAtualizada.setId(tarefaExistente.getId()); // Mantém o ID original
+                    return taskRepository.save(setTaskStatus(tarefaAtualizada));
+                })
+                .orElse(null);
     }
 
     public void deletarTarefa(Long id) {
@@ -61,31 +43,30 @@ public class TaskService {
     }
 
     public List<Task> obterTodasTarefas() {
-        List<Task> tarefas = taskRepository.findAll();
-        for (Task task : tarefas) {
-            setTaskStatus(task);
-        }
-        return tarefas;
+        return taskRepository.findAll().stream()
+                .map(this::setTaskStatus)
+                .collect(Collectors.toList());
     }
 
-    private void setTaskStatus(Task task) {
+    private Task setTaskStatus(Task task) {
         LocalDate currentDate = LocalDate.now();
+        TaskStatus status;
+
         if (task.getType() == TaskType.DATA) {
-            if (task.getDueDate().isBefore(currentDate)) {
-                task.setStatus(TaskStatus.ATRASADA);
-            } else if (task.getDueDate().isEqual(currentDate)) {
-                task.setStatus(TaskStatus.PREVISTA);
-            } else {
-                task.setStatus(TaskStatus.CONCLUIDA);
-            }
-        } else if (task.getType() == TaskType.PRAZO) {
-            // Lógica similar para tarefas do tipo Prazo
+            status = task.getDueDate().isBefore(currentDate) ? TaskStatus.ATRASADA :
+                    task.getDueDate().isEqual(currentDate) ? TaskStatus.PREVISTA :
+                            TaskStatus.CONCLUIDA;
         } else {
-            if (task.getCompleted()) {
-                task.setStatus(TaskStatus.CONCLUIDA);
-            } else {
-                task.setStatus(TaskStatus.PREVISTA);
-            }
+            status = task.getCompleted() ? TaskStatus.CONCLUIDA : TaskStatus.PREVISTA;
+        }
+
+        task.setStatus(status);
+        return task;
+    }
+
+    private void validarDataDeVencimento(Task tarefa) {
+        if (tarefa.getType() == TaskType.DATA && tarefa.getDueDate().isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("O vencimento das tarefas do tipo 'Data' deve ser igual ou após a data atual.");
         }
     }
 }
