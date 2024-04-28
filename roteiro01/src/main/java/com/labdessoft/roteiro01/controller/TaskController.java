1fdf6987ce8fce9a19fef51aa6308e9bce91c79e
Raul Fernandes <103540662+RaulFernandesGoulart@users.noreply.github.com>
2024-04-28 19:59:10 -0300

    Update TaskController.java

--- roteiro01/src/main/java/com/labdessoft/roteiro01/controller/TaskController.java
+++ roteiro01/src/main/java/com/labdessoft/roteiro01/controller/TaskController.java
@@ -1,14 +1,14 @@
 package com.labdessoft.roteiro01.controller;
 
 import com.example.roteiro01.entity.Task;
-import com.example.roteiro01.service.TaskService;
 import com.example.roteiro01.model.ErrorMessage;
+import com.example.roteiro01.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,45 +18,40 @@ public class TaskController {
     @Autowired
     private TaskService taskService;
 
+    // Obtém uma tarefa por ID
     @GetMapping("/{id}")
     public ResponseEntity<Task> obterTarefa(@PathVariable Long id) {
         Task tarefa = taskService.obterTarefaPorId(id);
-        if (tarefa != null) {
-            return ResponseEntity.ok(tarefa);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return tarefa != null ? ResponseEntity.ok(tarefa) : ResponseEntity.notFound().build();
     }
 
+    // Cria uma nova tarefa
     @PostMapping
     public ResponseEntity<?> criarTarefa(@Valid @RequestBody Task tarefa) {
         try {
             Task tarefaCriada = taskService.criarTarefa(tarefa);
-            URI location = URI.create("/tasks/" + tarefaCriada.getId());
-            return ResponseEntity.created(location).body(tarefaCriada);
+            return ResponseEntity.status(HttpStatus.CREATED)
+                                 .body(tarefaCriada);
         } catch (IllegalArgumentException e) {
-            ErrorMessage errorMessage = new ErrorMessage(e.getMessage());
-            return ResponseEntity.badRequest().body(errorMessage);
+            return ResponseEntity.badRequest().body(new ErrorMessage(e.getMessage()));
         }
     }
 
-
+    // Atualiza uma tarefa existente por ID
     @PutMapping("/{id}")
     public ResponseEntity<Task> atualizarTarefa(@PathVariable Long id, @Valid @RequestBody Task tarefa) {
         Task tarefaAtualizada = taskService.atualizarTarefa(id, tarefa);
-        if (tarefaAtualizada != null) {
-            return ResponseEntity.ok(tarefaAtualizada);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return tarefaAtualizada != null ? ResponseEntity.ok(tarefaAtualizada) : ResponseEntity.notFound().build();
     }
 
+    // Deleta uma tarefa existente por ID
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarTarefa(@PathVariable Long id) {
         taskService.deletarTarefa(id);
         return ResponseEntity.noContent().build();
     }
 
+    // Obtém todas as tarefas
     @GetMapping
     public ResponseEntity<List<Task>> obterTodasTarefas() {
         List<Task> tarefas = taskService.obterTodasTarefas();
