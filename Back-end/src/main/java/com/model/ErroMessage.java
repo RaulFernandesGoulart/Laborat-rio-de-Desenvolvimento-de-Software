60ea04b5ed6e8ee19bf998e9bef86c83ddb30b9f
Raul Fernandes <103540662+RaulFernandesGoulart@users.noreply.github.com>
2024-06-20 17:04:44 -0300

    att-back

--- Back-end/src/main/java/com/model/ErroMessage.java
+++
@@ -1,9 +0,0 @@
-package com.labdessoft.roteiro01.model;
-
-public class ErrorMessage {
-    public final String message;
-
-    public ErrorMessage(String message) {
-        this.message = message;
-    }
-}
